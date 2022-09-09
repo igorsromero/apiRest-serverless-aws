28ffa346e61dafb172f6ddef4bc200ea02a3945a
Igor Solfa Romero <igorsolfaromero@gmail.com>
2022-09-08 21:32:39 -0300

    feat: module 2

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # apiRest-serverless-aws
  
+### Criar um projeto serverless
+```
+serverless create --template aws-nodejs
+```
+
+### Rodar função localmente
+```
+sls invoke local -f hello
+```
+
+### Rodar função localmente e passando parâmetros pelo event
+```
+sls invoke local -f listarPacientes -d '{"teste": "teste"}'
+```
+
+### Realizando deploy
+```
+sls deploy
+```
--- handler.js
+++ handler.js
@@ -1,12 +1,18 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+const pacientes = [
+  {id: 1, nome: 'João', dataNascimento: '1990-01-01'},
+  {id: 2, nome: 'Maria', dataNascimento: '1990-01-01'},
+  {id: 3, nome: 'José', dataNascimento: '1990-01-01'},
+]
+
+module.exports.listarPacientes = async (event) => {
+  console.log(event)
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        pacientes
       },
       null,
       2
