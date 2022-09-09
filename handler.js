6f08635a314b06bb3f351192dfd1b5f0f2b7ce38
Igor Solfa Romero <igorsolfaromero@gmail.com>
2022-09-09 08:55:28 -0300

    feat: add update patient endpoint

--- handler.js
+++ handler.js
@@ -111,3 +111,59 @@ module.exports.cadastrarPaciente = async (event) => {
     };
   }
 };
+
+module.exports.atualizarPaciente = async (event) => {  
+  const { pacienteId } = event.pathParameters
+
+  try {
+    const timestamp = new Date().getTime();
+
+    let dados = JSON.parse(event.body);
+
+    const { nome, data_nascimento, email, telefone } = dados;
+
+    await dynamoDB
+      .update({
+        ...params,
+        Key: {
+          paciente_id: pacienteId
+        },
+        UpdateExpression:
+          'SET nome = :nome, data_nascimento = :dt, email = :email,' 
+          + ' telefone = :telefone, atualizado_em = :atualizado_em',
+        ConditionExpression: 'attribute_exists(paciente_id)',
+        ExpressionAttributeValues: {
+          ':nome': nome,
+          ':dt': data_nascimento,
+          ':email': email,
+          ':telefone': telefone,
+          ':atualizado_em': timestamp
+        }
+      })
+      .promise()
+
+    return {
+      statusCode: 204,
+    };
+  } catch (err) {
+    console.log("Error", err);
+
+    let error = err.name ? err.name : "Exception";
+    let message = err.message ? err.message : "Unknown error";
+    let statusCode = err.statusCode ? err.statusCode : 500;
+
+    if (error == 'ConditionalCheckFailedException') {
+      error = 'Paciente não existe';
+      message = `Recurso com o ID ${pacienteId} não existe e não pode ser atualizado`;
+      statusCode = 404;
+    }
+
+    return {
+      statusCode,
+      body: JSON.stringify({
+        error,
+        message
+      }),
+    };
+  }
+};
